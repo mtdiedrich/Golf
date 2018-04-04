8a515a43e3ace5e3b58d1a8c8a5f38f86523f0b1
mtdiedrich <mitchdiedrich@gmail.com>
2018-04-04 17:59:58 -0500

    commit

--- ProbabilityComparison.py
+++ ProbabilityComparison.py
@@ -16,15 +16,38 @@ def openFile(file):
 
 
 def main():
-    file1 = 'DustinJohnsonProbabilities.csv'
-    file2 = 'JordanSpiethProbabilities.csv'
-    golfer1 = openFile(file1)
-    golfer2 = openFile(file2)
-    sum = 0
+
+    golfer1 = openFile('DustinJohnsonProbabilities.csv')
+    golfer2 = openFile('JustinThomasProbabilities.csv')
+    golfer3 = openFile('JordanSpiethProbabilities.csv')
+    golfers = [golfer1, golfer2, golfer3]
+
+    sum  = 0
     for x in range(0,len(golfer1)):
-        sum += float(golfer1[x][2]) * float(golfer2[x-1][3])
+        for y in range(0,len(golfer2)):
+            for z in range(0, min(x,y)):
+                sum += float(golfer1[x][2]) * float(golfer2[y][2]) * float(golfer3[z][2])
+                print(sum)
     print(sum)
 
+    sum2beats1 = 0
+    for x in range(0,len(golfer1)):
+        for y in range(0,x):
+            sum2beats1 += float(golfer1[x][2]) * float(golfer2[y][2])
+    print(sum2beats1-1)
+
+    sum1beats2 = 0
+    for x in range(0,len(golfer2)):
+        for y in range(0,x):
+            sum1beats2 += float(golfer2[x][2]) * float(golfer1[y][2])
+    print(sum1beats2-1)
+
+    sumTie = 0
+    for x in range(0,len(golfer1)):
+        sumTie += float(golfer1[x][2]) * float(golfer2[x][2])
+    print(sumTie-1)
+    
+    print(sum2beats1 + sum1beats2 + sumTie - 3)
 
 if __name__ == "__main__":
     main()
