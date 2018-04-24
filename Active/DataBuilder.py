2893bb678106933146f6b2b2815736b6048cfe5b
mtdiedrich <mitchdiedrich@gmail.com>
2018-04-24 17:21:14 -0500

    commit

--- Active/DataBuilder.py
+++ Active/DataBuilder.py
@@ -5,10 +5,11 @@ sys.path.insert(0, r'C:\Users\Mitch\Projects\PycharmProjects\darkskylib-master')
 from darksky import forecast
 
 def main():
-    BOSTON = ('6c02739879c5b450bb2f2794ba61ddeb', 41.3949, -91.3147)
-    time = dt.datetime(2018,4,20,12).isoformat()
-    boston = forecast(*BOSTON,time=time)
-    print(boston.temperature)
+
+    IC = ('6c02739879c5b450bb2f2794ba61ddeb', 41.3949, -91.3147)
+    time = dt.datetime(2018,4,24,16).isoformat()
+    iowa_city = forecast(*IC,time=time)
+    print(iowa_city.temperature, iowa_city.windSpeed, iowa_city.windGust)
 
 if __name__ == "__main__":
     main()
