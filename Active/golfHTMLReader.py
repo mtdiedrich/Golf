2296dd62226b45b92f7a48fe11cd964aa921f8eb
mtdiedrich <mitchdiedrich@gmail.com>
2018-05-01 00:58:02 -0500

    commit

--- Active/golfHTMLReader.py
+++ Active/golfHTMLReader.py
@@ -10,44 +10,58 @@ def find_nth(haystack, needle, n):
 
 def main():
 
-    file_object = open(file = r'C:\Users\Mitch\Projects\PycharmProjects\Golf\Data\HTML Data\SafewayOpen2007.html',mode = 'r').read()
-    x = 1
-    golfers = []
+    #TO DO
+    #Each entry must be labelled with course name and tournament name
+    #Eventually must add geolocation data
+    #Afterwards must add weather data from DarkSky API
+    #Must combine all events for each individual golfer
 
-    while find_nth(file_object,'<td class="cell">',x) != -1:
+    files = [r'C:\Users\Mitch\Projects\PycharmProjects\Golf\Data\HTML Data\SafewayOpen2007.html']
 
-        start_string = '<td class="cell">'
-        nth_string = find_nth(file_object,start_string,x)
-        end_string_index = file_object[nth_string:].find('</td>')
-        golfer = file_object[nth_string+len(start_string)+21:nth_string+end_string_index]
-        round_scores = file_object[nth_string+end_string_index+35:nth_string+end_string_index+240]
+    for x in files:
+        file_object = open(file=x,mode='r').read()
+        x = 1
+        golfers = []
 
-        first_round_score = round_scores[find_nth(round_scores,'>',1)+1:find_nth(round_scores,'<',2)]
-        second_round_score = round_scores[find_nth(round_scores,'>',3)+1:find_nth(round_scores,'<',4)]
-        third_round_score = round_scores[find_nth(round_scores, '>', 5) + 1:find_nth(round_scores, '<', 6)]
-        fourth_round_score = round_scores[find_nth(round_scores, '>', 7) + 1:find_nth(round_scores, '<', 8)]
+        while find_nth(file_object, '<td class="cell">', x) != -1:
 
-        if first_round_score != "":
-            first_round_score = int(first_round_score)
-        if second_round_score != "":
-            second_round_score = int(second_round_score)
-        if third_round_score != "":
-            third_round_score = int(third_round_score)
-        if fourth_round_score != "":
-            fourth_round_score = int(fourth_round_score)
+            start_string = '<td class="cell">'
+            nth_string = find_nth(file_object, start_string, x)
+            end_string_index = file_object[nth_string:].find('</td>')
+            golfer = file_object[nth_string + len(start_string) + 21:nth_string + end_string_index]
+            round_scores = file_object[nth_string + end_string_index + 35:nth_string + end_string_index + 240]
 
-        golfers += [[golfer, first_round_score,second_round_score,third_round_score,fourth_round_score]]
-        x += 1
+            first_round_score = round_scores[find_nth(round_scores, '>', 1) + 1:find_nth(round_scores, '<', 2)]
+            second_round_score = round_scores[find_nth(round_scores, '>', 3) + 1:find_nth(round_scores, '<', 4)]
+            third_round_score = round_scores[find_nth(round_scores, '>', 5) + 1:find_nth(round_scores, '<', 6)]
+            fourth_round_score = round_scores[find_nth(round_scores, '>', 7) + 1:find_nth(round_scores, '<', 8)]
 
-    next_end = file_object[file_object.find("Ending:"):].find('<')
-    date_string = file_object[file_object.find("Ending:")+len("Ending: "):file_object.find("Ending:")+next_end]
-    year = int(date_string[find_nth(date_string,'/',2)+1:])
-    month = int(date_string[:find_nth(date_string,'/',1)])
-    day = int(date_string[find_nth(date_string,'/',1)+1:find_nth(date_string,'/',2)])
-    ending_date = dt.datetime(year,month,day)
+            if first_round_score != "":
+                first_round_score = int(first_round_score)
+            if second_round_score != "":
+                second_round_score = int(second_round_score)
+            if third_round_score != "":
+                third_round_score = int(third_round_score)
+            if fourth_round_score != "":
+                fourth_round_score = int(fourth_round_score)
 
-    print(date_string)
-    print(ending_date)
+            golfers += [[golfer, first_round_score, second_round_score, third_round_score, fourth_round_score]]
+            x += 1
+
+        next_end = file_object[file_object.find("Ending:"):].find('<')
+        date_string = file_object[file_object.find("Ending:") + len("Ending: "):file_object.find("Ending:") + next_end]
+        year = int(date_string[find_nth(date_string, '/', 2) + 1:])
+        month = int(date_string[:find_nth(date_string, '/', 1)])
+        day = int(date_string[find_nth(date_string, '/', 1) + 1:find_nth(date_string, '/', 2)])
+        fourth_date = dt.date(year, month, day)
+        first_date = fourth_date - dt.timedelta(days=3)
+        second_date = fourth_date - dt.timedelta(days=2)
+        third_date = fourth_date - dt.timedelta(days=1)
+
+        for x in golfers:
+            x += [str(first_date), str(second_date), str(third_date), str(fourth_date)]
+
+        print(pd.DataFrame(golfers))
 
 
 if __name__ == "__main__":
