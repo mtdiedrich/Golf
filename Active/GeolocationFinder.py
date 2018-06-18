4d44f633dc0bd0271480ec6fda8f7573aa365d49
mtdiedrich <mitchdiedrich@gmail.com>
2018-06-18 11:59:07 -0500

    Finished migrating to new format

--- Active/GeolocationFinder.py
+++ Active/GeolocationFinder.py
@@ -14,8 +14,8 @@ def add_geo_data(location, keyword):
     global google_places
 
     query_result = google_places.nearby_search(location=location,keyword=keyword, radius=50000)
-    df.iloc[x,2] = query_result.places[0].geo_location['lat']
-    df.iloc[x,3] = query_result.places[0].geo_location['lng']
+    df.iloc[x,2] = round(query_result.places[0].geo_location['lat'],7)
+    df.iloc[x,3] = round(query_result.places[0].geo_location['lng'],7)
     x += 1
 
 def main():
@@ -31,66 +31,23 @@ def main():
     add_geo_data('Irving, Texas', 'TPC Las Colinas')
     add_geo_data('St. Louis, Missouri', 'Bellerive Country Club')
     add_geo_data('Cherry Hills Village, Colorado', 'Cherry Hills Country Club')
+    add_geo_data('Fort Worth, Texas', 'Colonial Country Club')
+    add_geo_data('Memphis, Tennessee', 'TPC Southwind')
+    add_geo_data('Coal Valley, Illinois', 'Oakwood Country Club')
+    add_geo_data('Playa del Carmen', 'El Camaleon')
+    add_geo_data('Tulsa, Oklahoma', 'Southern Hills Country Club')
+    add_geo_data('Bloomfield Township, Michigan', 'Oakland Hills Country Club')
+    add_geo_data('Akron, Ohio','Firestone Country Club')
+    add_geo_data('Bethesda, Maryland','Congressional Country Club')
+    add_geo_data('Pittsford, New York','Oak Hill Country Club')
+    add_geo_data('Mamaroneck, New York','Winged Foot Country Club')
+    add_geo_data('Sammamish, Washington','Sahalee Country Club')
+    add_geo_data('Springfield Township, New Jersey','Baltusrol Golf Club')
+    add_geo_data('Mississaugua, Ontario','Mississaugua Golf and Country Club')
+    add_geo_data('Montreal, Quebec','Royal Montreal Golf Club')
+    
     '''
-    query_result = google_places.nearby_search(location='Fort Worth, Texas',keyword='Colonial Country Club', radius=50000)
-    df.iloc[x,2] = query_result.places[0].geo_location['lat']
-    df.iloc[x,3] = query_result.places[0].geo_location['lng']
-    x += 1
-    query_result = google_places.nearby_search(location='Memphis, Tennessee',keyword='TPC Southwind', radius=50000)
-    df.iloc[x,2] = query_result.places[0].geo_location['lat']
-    df.iloc[x,3] = query_result.places[0].geo_location['lng']
-    x += 1
-    query_result = google_places.nearby_search(location='Coal Valley, Illinois',keyword='Oakwood Country Club', radius=50000)
-    df.iloc[x,2] = query_result.places[0].geo_location['lat']
-    df.iloc[x,3] = query_result.places[0].geo_location['lng']
-    x += 1
-    query_result = google_places.nearby_search(location='Playa del Carmen',keyword='El Camaleon', radius=50000)
-    df.iloc[x,2] = query_result.places[0].geo_location['lat']
-    df.iloc[x,3] = query_result.places[0].geo_location['lng']
-    x += 1
-    query_result = google_places.nearby_search(location='Tulsa, Oklahoma',keyword='Southern Hills Country Club', radius=50000)
-    df.iloc[x,2] = query_result.places[0].geo_location['lat']
-    df.iloc[x,3] = query_result.places[0].geo_location['lng']
-    x += 1
-    query_result = google_places.nearby_search(location='Bloomfield Township, Michigan',keyword='Oakland Hills Country Club', radius=50000)
-    df.iloc[x,2] = query_result.places[0].geo_location['lat']
-    df.iloc[x,3] = query_result.places[0].geo_location['lng']
-    x += 1
-    query_result = google_places.nearby_search(location='Akron, Ohio',keyword='Firestone Country Club', radius=50000)
-    df.iloc[x,2] = query_result.places[0].geo_location['lat']
-    df.iloc[x,3] = query_result.places[0].geo_location['lng']
-    x += 1
-    query_result = google_places.nearby_search(location='Bethesda, Maryland',keyword='Congressional Country Club', radius=50000)
-    df.iloc[x,2] = query_result.places[0].geo_location['lat']
-    df.iloc[x,3] = query_result.places[0].geo_location['lng']
-    x += 1
-    query_result = google_places.nearby_search(location='Pittsford, New York',keyword='Oak Hill Country Club', radius=50000)
-    df.iloc[x,2] = query_result.places[0].geo_location['lat']
-    df.iloc[x,3] = query_result.places[0].geo_location['lng']
-    x += 1
-    query_result = google_places.nearby_search(location='Mamaroneck, New York',keyword='Winged Foot Country Club', radius=50000)
-    df.iloc[x,2] = query_result.places[0].geo_location['lat']
-    df.iloc[x,3] = query_result.places[0].geo_location['lng']
-    x += 1
-    query_result = google_places.nearby_search(location='Sammamish, Washington',keyword='Sahalee Country Club', radius=50000)
-    df.iloc[x,2] = query_result.places[0].geo_location['lat']
-    df.iloc[x,3] = query_result.places[0].geo_location['lng']
-    x += 1
-    query_result = google_places.nearby_search(location='Springfield Township, New Jersey',keyword='Baltusrol Golf Club', radius=50000)
-    df.iloc[x,2] = round(query_result.places[0].geo_location['lat'],7)
-    df.iloc[x,3] = round(query_result.places[0].geo_location['lng'],7)
-    x += 1
-    query_result = google_places.nearby_search(location='Mississaugua, Ontario',keyword='Mississaugua Golf and Country Club', radius=50000)
-    df.iloc[x,2] = round(query_result.places[0].geo_location['lat'],7)
-    df.iloc[x,3] = round(query_result.places[0].geo_location['lng'],7)
-    x += 1
     query_result = google_places.nearby_search(location='Montreal, Quebec',keyword='Royal Montreal Golf Club', radius=50000)
-    df.iloc[x,2] = round(query_result.places[0].geo_location['lat'],7)
-    df.iloc[x,3] = round(query_result.places[0].geo_location['lng'],7)
-    x += 1
-    query_result = google_places.nearby_search(location='Montreal, Quebec',keyword='Royal Montreal Golf Club', radius=50000)
-    df.iloc[x,2] = round(query_result.places[0].geo_location['lat'],7)
-    df.iloc[x,3] = round(query_result.places[0].geo_location['lng'],7)
     for place in query_result.places:
         # Returned places from a query are place summaries.
         print(place.name)
