bd522bdfe219323596bccc1a068c2d0338235338
mtdiedrich <mitchdiedrich@gmail.com>
2018-06-22 15:08:53 -0500

    changed for loop to use iterrows

--- link_golfer_courses.py
+++ link_golfer_courses.py
@@ -5,17 +5,16 @@ pd.set_option('display.width',1000)
 
 def main():
     gdf = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\golfers.csv',index_col = 'Unnamed: 0',encoding = 'latin1')
-    ndf = pd.DataFrame()
-    for x in range(len(gdf['0'])):
-        round_one = pd.Series([gdf.iloc[x].loc['0'], gdf.iloc[x].loc['1'], gdf.iloc[x].loc['2'], gdf.iloc[x].loc['3'], gdf.iloc[x].loc['4']])
-        round_two = pd.Series([gdf.iloc[x].loc['0'], gdf.iloc[x].loc['1'], gdf.iloc[x].loc['2'], gdf.iloc[x].loc['5'], gdf.iloc[x].loc['6']])
-        round_three = pd.Series([gdf.iloc[x].loc['0'], gdf.iloc[x].loc['1'], gdf.iloc[x].loc['2'], gdf.iloc[x].loc['7'], gdf.iloc[x].loc['8']])
-        round_four = pd.Series([gdf.iloc[x].loc['0'], gdf.iloc[x].loc['1'], gdf.iloc[x].loc['2'], gdf.iloc[x].loc['9'], gdf.iloc[x].loc['10']])
-        ndf.append(round_one, ignore_index=True)
-        ndf.append(round_two, ignore_index=True)
-        ndf.append(round_three, ignore_index=True)
-        ndf.append(round_four,ignore_index=True)
+    rounds = []
+    x = 0
+    for index, row in gdf.iterrows():
+        x += 1
+        rounds += [[row['0'],row['1'],row['2'],row['3'],row['4']]]
+        rounds += [[row['0'],row['1'],row['2'],row['5'],row['6']]]
+        rounds += [[row['0'],row['1'],row['2'],row['7'],row['8']]]
+        rounds += [[row['0'],row['1'],row['2'],row['9'],row['10']]]
         print("Percent Done: " + str(100 * float(x)/len(gdf['0'])))
+    ndf = pd.DataFrame(rounds)
     print(ndf)
         
 if __name__ == "__main__":
