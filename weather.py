da73db381bb5c4b23bf6b83824f86511e1b25500
mtdiedrich <mitchdiedrich@gmail.com>
2018-06-25 15:33:11 -0500

    weather calls

--- weather.py
+++ weather.py
@@ -16,13 +16,20 @@ def number_string_to_int(number):
     return int(number)
 
 def main():
+    key = input("Key: ")
     df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\courses_dates_geodata.csv', index_col = 'Unnamed: 0')
+    print(df)
     weather_data = []
+    count = 0
     for index, row in df.iterrows():
+        count += 1
         lat = row['2']
         lng = row['3']
-        date = string_to_date(row['1'])
-        print(date)
+        date = string_to_date(row['1']).isoformat()
+        with ds.forecast(key,lat,lng,time=date) as location_weather:
+            print(location_weather.temperature)
+        if count > 0:
+            break
 
 if __name__=="__main__":
     main()
