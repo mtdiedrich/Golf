45bacc0d5a328197203e32c80b7ac80cabfa32cf
mtdiedrich <mitchdiedrich@gmail.com>
2018-06-26 10:04:08 -0500

    secured weather

--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ def string_to_date(date):
     year = number_string_to_int(date[0:4])
     month = number_string_to_int(date[5:7])
     day = number_string_to_int(date[8:10])
-    time = dt(year,month,day,12)
+    time = dt(year,month,day)
     return time
 
 def number_string_to_int(number):
@@ -26,9 +26,14 @@ def main():
         lat = row['2']
         lng = row['3']
         date = string_to_date(row['1']).isoformat()
-        with ds.forecast(key,lat,lng,time=date) as location_weather:
-            print(location_weather.temperature)
-        if count > 0:
+        with ds.forecast(key,lat,lng,time=date) as lw:
+            weather = [lw.summary,lw.precipIntensity,lw.precipProbability,
+                       lw.precipType,lw.temperature,lw.apparentTemperature,
+                       lw.dewPoint,lw.humidity,lw.pressure,lw.windSpeed,
+                       lw.windGust,lw.windBearing,lw.cloudCover,lw.visibility]
+            print(weather)
+
+        if count > 2:
             break
 
 if __name__=="__main__":
