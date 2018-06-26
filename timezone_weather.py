a7f091d6262a7e75b246f33573638764fe0bbb1b
mtdiedrich <mitchdiedrich@gmail.com>
2018-06-26 15:44:09 -0500

    timezones collected

--- timezone_weather.py
+++ timezone_weather.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import googlemaps
-import googlemaps.timezone
+import googlemaps.timezone as tz
 from datetime import datetime as dt
 
 pd.set_option('display.max_columns',20)
 pd.set_option('display.width',1000)
+GMAPS = googlemaps.Client(key=input("Key: ")) #input Google API key
 
 def string_to_date(date):
     #Given the date in the format it is found in the data, this turns each
@@ -26,13 +27,23 @@ def number_string_to_int(number):
 def geo_tuple(row):
     lat = row[2]
     lng = row[3]
-    return tuple(lat,lng)
+    return tuple((lat,lng))
+
+def entry_in_tuple(pair, entry):
+    return(pair(entry))
 
 def main():
     
     df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\courses_dates_geodata.csv', index_col = 'Unnamed: 0')
-    #gmaps = googlemaps.Client(key=input("Key: ")) #input Google API key
-    df['Pairs'] = df.apply(lambda row: geo_tuple(row))
+    df['Pairs'] = df.apply(lambda row: geo_tuple(row), axis = 1)
+    geodata = list(set(list(df['Pairs'])))
+    timezones = []
+    for x in range(len(geodata)):
+        timezone = tz.timezone(GMAPS,geodata[x])
+        timezones += [[geodata[x][0],geodata[x][1],timezone['timeZoneId'],timezone['timeZoneName']]]
+    tdf = pd.DataFrame(timezones)
+    print(tdf)
+    tdf.to_csv(r'C:\Users\Mitch\Projects\Golf\Data\timezones.csv')
 
 if __name__=="__main__":
     main()
