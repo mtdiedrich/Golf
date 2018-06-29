533bc57a9c14b6ccf9d60af22077058fa24e90b7
mtdiedrich <mitchdiedrich@gmail.com>
2018-06-29 15:56:27 -0500

    DataFrame merging

--- weather_round_link.py
+++ weather_round_link.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
-pd.set_option('display.max_columns',20)
-pd.set_option('display.width', 1000)
-
-
 def main():
     df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\weather_data_0_through_899.csv')
     rdf = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\individual_rounds.csv',index_col='Unnamed: 0',encoding='latin1')
     df.columns = ['Course','Date'] + list(df.columns[2:])
-    rdf.drop(['index'],axis=1)
+    rdf = rdf.drop(['index'],axis=1)
+    rdf.columns = ['Golfer','Tournament','Course','Date','Score','Latitude','Longitude']
+    mdf = rdf.merge(df, how = 'left', on = ['Date', 'Latitude'])
     print(rdf)
+    print(mdf)
 
 
 if __name__=="__main__":
