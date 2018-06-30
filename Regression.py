404e99a55c3190ebe705a075edfd00f4447d127c
mtdiedrich <mitchdiedrich@gmail.com>
2018-06-30 11:45:06 -0500

    Regression exploration

--- Regression.py
+++ Regression.py
@@ -1,7 +1,10 @@
+import random
+import math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 
 pd.set_option('display.max_columns',20)
 pd.set_option('display.width',1000)
@@ -9,23 +12,36 @@ pd.set_option('display.width',1000)
 def main():
 
     df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\full_data.csv',encoding='latin1')
+    df = df.rename(columns={'Precipitation Intensity':'Precip Intens','Precipitation Probability':'Precip Prob'})
 
-    features = [f for f in df.columns][8:]
-    features = [features[0]] + [features[2]] + features[4:]
-    fdf = df[features]
-    fdf.columns = ['Precip Intens','Precip Prob'] + list(fdf.columns[2:])
-    corr_mat = fdf.corr()
-    print(corr_mat)
+    features = [f for f in df.columns]
+    features = [features[4]] + [features[8]] + [features[10]] + features[12:]
+
+    ndf = df[features]
+    ndf = ndf.dropna()
+    ndf = ndf.reset_index(drop=True)
+    ndf = ndf.sample(frac=1)
+    
+    #corr_mat = ndf.corr()
+    #print(corr_mat)
+
+    x = ndf[features[1:]]
+    x = np.asarray(x).reshape(x.shape[0],x.shape[1])
+    y = ndf[features[0]]
+    y = np.asarray(y).reshape(y.shape[0],1)
 
     linreg = linear_model.LinearRegression()
-    ndf = df[['Score','Precipitation Intensity','Wind Speed']]
-    ndf = ndf.dropna()
-    x = ndf[['Precipitation Intensity','Wind Speed']].reset_index(drop=True)
-    x = np.asarray(x).reshape(x.shape[0],2)
-    y = ndf['Score'].reset_index(drop=True)
-    y = np.asarray(y).reshape(y.shape[0],1)
     linreg.fit(x,y)
-    print(linreg.intercept_, linreg.coef_)
+
+    pred = linreg.predict(x)
+    residuals = pred - y
+   
+    print(np.mean(residuals))
+    print(np.std(residuals))
+    print(np.histogram(residuals))
+    
+    #mse = mean_squared_error(test_y, pred)
+    #print(linreg.intercept_,linreg.coef_,mse)
 
 if __name__=="__main__":
     main()
