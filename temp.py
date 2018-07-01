6fe6c3874067ab41c1601e17f8fab8b5ccdf0dfe
mtdiedrich <mitchdiedrich@gmail.com>
2018-07-01 12:00:51 -0500

    data collection

--- temp.py
+++ temp.py
@@ -1,12 +1,15 @@
+import math
 import pandas as pd
 from operator import itemgetter
 
 pd.set_option('display.max_columns',20)
+#pd.set_option('display.max_rows',100000)
 pd.set_option('display.width',1000)
+pd.set_option('display.max_colwidth',25)
 
 def main():
 
-    df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\golfers.csv', index_col='Unnamed: 0', encoding = 'latin1')
+    df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\golfers.csv',index_col='Unnamed: 0',encoding='latin1')
     df.columns = ['Golfer','Tournament','Course','Date 1','Score 1','Date 2','Score 2','Date 3','Score 3','Date 4','Score 4']
     df1 = df[['Golfer','Tournament','Course','Date 1','Score 1']]
     df2 = df[['Golfer','Tournament','Course','Date 2','Score 2']]
@@ -15,8 +18,24 @@ def main():
     df1.columns = df2.columns = df3.columns = df4.columns = ['Golfer','Tournament','Course','Date','Score']
     df = pd.concat([df1,df2,df3,df4])
     df = df.reset_index(drop=True)
-    print(df)
+    df.to_csv(r'C:\Users\Mitch\Projects\Golf\Data\individual_rounds.csv',index=False)
+
+    cdf = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\Courses.csv', index_col = 'Unnamed: 0')
+    cdf = cdf[['Course Name','Latitude','Longitude']]
+    cdf.columns = ['Course','Latitude','Longitude']
+    mdf = df.merge(cdf,how='outer',on=['Course'])
+    mdf = mdf.reset_index(drop=True)
+
+    tdf = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\timezones.csv',index_col='Unnamed: 0',encoding='latin1')
+    tdf.columns = ['Latitude','Longitude','Region','Timezone','To UTC']
     
+    cdf = mdf.merge(tdf,how='outer',on=['Latitude','Longitude'])
+    cdf = cdf.drop(['Region'],axis=1)
+
+    df = cdf[cdf['Latitude'].isnull()]
+    print(df)
+
+
 if __name__=="__main__":
     main()
 
