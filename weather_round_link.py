3e6595069b6fccbd20076b6b57a45379a007217b
mtdiedrich <mitchdiedrich@gmail.com>
2018-07-03 12:40:27 -0500

    reorganization

--- weather_round_link.py
+++ weather_round_link.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
 def main():
+
     df1 = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\weather_data_0_through_899.csv')
-    df1 = df1.rename(index=str,columns={'Longiude':'Longitude'})
     df2 = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\weather_data_900_through_1799.csv')
-    df2 = df2.rename(index=str,columns={'Longiude':'Longitude'})
     df3 = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\weather_data_1800_through_2699.csv')
-    df3 = df2.rename(index=str,columns={'Longiude':'Longitude'})
+
+    df1 = df1.rename(index=str,columns={'Longiude':'Longitude'})
+    df2 = df2.rename(index=str,columns={'Longiude':'Longitude'})
+    df3 = df3.rename(index=str,columns={'Longiude':'Longitude'})
+
     df = pd.concat([df1,df2,df3])
     rdf = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\individual_rounds.csv',encoding='latin1')
     df.columns = ['Course','Date'] + list(df.columns[2:])
