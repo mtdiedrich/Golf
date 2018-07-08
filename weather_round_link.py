6380e8a66fcb9de621c2e40b5d1eb192e9c16716
mtdiedrich <mitchdiedrich@gmail.com>
2018-07-07 23:04:34 -0500

    found a problem

--- weather_round_link.py
+++ weather_round_link.py
@@ -19,12 +19,15 @@ def main():
     df = pd.concat([df1,df2,df3,df4,df5,df6])
     rdf = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\individual_rounds.csv',encoding='latin1')
     df.columns = ['Course','Date'] + list(df.columns[2:])
-    mdf = df.merge(rdf, how = 'outer', on = ['Course','Date'])
-    cols = list(mdf.columns[:len(mdf.columns)-3]) + list(mdf.columns[len(mdf.columns)-3:])     
-    mdf = mdf[cols]
-    print(mdf)
+    mdf = df.merge(rdf, how = 'right', on = ['Course','Date'])
+    cols = list(mdf.columns[-3:-1]) + [mdf.columns[-1]] + list(mdf.columns[0:-3])
+    #mdf = mdf[cols]
+    #print(mdf)
     mdf.to_csv(r'C:\Users\Mitch\Projects\Golf\Data\full_data.csv', index=False)
-
+    df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\full_data.csv',encoding='latin1')
+    print(df)
+    #There's a Memory Error thrown when reading in this csv
+    #It will have to be handled, but it seems like a mess
 
 if __name__=="__main__":
     main()
