8ba48835aef0ba472bae84a50f2f4ec6568e1946
mtdiedrich <mitchdiedrich@gmail.com>
2018-07-08 17:05:36 -0500

    temp reorganization

--- temp.py
+++ temp.py
@@ -15,35 +15,48 @@ def main():
     
     #Order
     #Read in CSVs
+    #You are here
     #Match rows to geodata
     #Add geodata to rows w/null values via similar()
     #Separate into individual rounds
     #Ship
-    df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\golfers.csv',index_col='Unnamed: 0',encoding='latin1')
-    df.columns = ['Golfer','Tournament','Course','Date 1','Score 1','Date 2','Score 2','Date 3','Score 3','Date 4','Score 4']
-    df1 = df[['Golfer','Tournament','Course','Date 1','Score 1']]
-    df2 = df[['Golfer','Tournament','Course','Date 2','Score 2']]
-    df3 = df[['Golfer','Tournament','Course','Date 3','Score 3']]
-    df4 = df[['Golfer','Tournament','Course','Date 4','Score 4']]
-    df1.columns = df2.columns = df3.columns = df4.columns = ['Golfer','Tournament','Course','Date','Score']
-    df = pd.concat([df1,df2,df3,df4])
-    df = df.reset_index(drop=True)
-    df.to_csv(r'C:\Users\Mitch\Projects\Golf\Data\individual_rounds.csv',index=False)
+    
+    gdf = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\golfers.csv',index_col='Unnamed: 0',encoding='latin1')
+    gdf.columns = ['Golfer','Tournament','Course','Date 1','Score 1','Date 2','Score 2','Date 3','Score 3','Date 4','Score 4']
 
     cdf = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\Courses.csv', index_col = 'Unnamed: 0')
-    cdf = cdf[['Course Name','Latitude','Longitude']]
-    cdf.columns = ['Course','Latitude','Longitude']
-    mdf = df.merge(cdf,how='outer',on=['Course'])
-    mdf = mdf.reset_index(drop=True)
+    cdf.columns = ['Course','Score','Latitude','Longitude'] 
 
     tdf = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\timezones.csv',index_col='Unnamed: 0',encoding='latin1')
-    tdf.columns = ['Latitude','Longitude','Region','Timezone','To UTC']
-    
-    ndf = mdf.merge(tdf,how='outer',on=['Latitude','Longitude'])
-    ndf = ndf.drop(['Region'],axis=1)
+    tdf.columns = ['Latitude','Longitude','Region','Timezone','UTC Offset'] 
 
-    null_df = df[df['Latitude'].isnull()]
-    null_df.apply(lambda row: lat_sim(row),axis=1)
+    print(tdf)
+
+    #df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\golfers.csv',index_col='Unnamed: 0',encoding='latin1')
+    #df.columns = ['Golfer','Tournament','Course','Date 1','Score 1','Date 2','Score 2','Date 3','Score 3','Date 4','Score 4']
+    #df1 = df[['Golfer','Tournament','Course','Date 1','Score 1']]
+    #df2 = df[['Golfer','Tournament','Course','Date 2','Score 2']]
+    #df3 = df[['Golfer','Tournament','Course','Date 3','Score 3']]
+    #df4 = df[['Golfer','Tournament','Course','Date 4','Score 4']]
+    #df1.columns = df2.columns = df3.columns = df4.columns = ['Golfer','Tournament','Course','Date','Score']
+    #df = pd.concat([df1,df2,df3,df4])
+    #df = df.reset_index(drop=True)
+    #df.to_csv(r'C:\Users\Mitch\Projects\Golf\Data\individual_rounds.csv',index=False)
+
+    #cdf = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\Courses.csv', index_col = 'Unnamed: 0')
+    #cdf = cdf[['Course Name','Latitude','Longitude']]
+    #cdf.columns = ['Course','Latitude','Longitude']
+    #mdf = df.merge(cdf,how='outer',on=['Course'])
+    #mdf = mdf.reset_index(drop=True)
+
+    #tdf = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\timezones.csv',index_col='Unnamed: 0',encoding='latin1')
+    #tdf.columns = ['Latitude','Longitude','Region','Timezone','To UTC']
+    
+    #ndf = mdf.merge(tdf,how='outer',on=['Latitude','Longitude'])
+    #ndf = ndf.drop(['Region'],axis=1)
+
+    #null_df = df[df['Latitude'].isnull()]
+    #null_df.apply(lambda row: lat_sim(row),axis=1)
     #df['Latitude'] = df.apply(lambda row: temp_fun(row))
     #df['Longitude'] = df.apply(lambda row: temp_fun(row))
     #df['Timezone'] = df.apply(lambda row: temp_fun(row))
