584da703fd5246cb4120762723572cc129a667bb
mtdiedrich <mitchdiedrich@gmail.com>
2018-07-10 08:45:53 -0500

    individual rounds handled

--- temp.py
+++ temp.py
@@ -36,14 +36,9 @@ def create_correction_dict(row):
 
 def main():
     
-    #Order
-    #Read in CSVs
-    #Match rows to geodata
-    #You are here
-    #Add geodata to rows w/null values via similar()
-    #Separate into individual rounds
-    #Ship
-    
+    #Link with timezone data
+    #Link w/weather data (may need to redo API calls)
+
     #Suppress warnings
     #remove unncessary DataFrames
 
@@ -79,18 +74,17 @@ def main():
 
     df = df.dropna(subset=['Golfer'])
 
-    print(df)
+    df1 = df[['Golfer','Tournament','Course','Date 1','Score 1']]
+    df2 = df[['Golfer','Tournament','Course','Date 2','Score 2']]
+    df3 = df[['Golfer','Tournament','Course','Date 3','Score 3']]
+    df4 = df[['Golfer','Tournament','Course','Date 4','Score 4']]
+    df1.columns = df2.columns = df3.columns = df4.columns = ['Golfer','Tournament','Course','Date','Score']
+    df = pd.concat([df1,df2,df3,df4])
+    df = df.dropna(subset=['Score'])
+    df = df.reset_index(drop=True)
 
-    #df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\golfers.csv',index_col='Unnamed: 0',encoding='latin1')
-    #df.columns = ['Golfer','Tournament','Course','Date 1','Score 1','Date 2','Score 2','Date 3','Score 3','Date 4','Score 4']
-    #df1 = df[['Golfer','Tournament','Course','Date 1','Score 1']]
-    #df2 = df[['Golfer','Tournament','Course','Date 2','Score 2']]
-    #df3 = df[['Golfer','Tournament','Course','Date 3','Score 3']]
-    #df4 = df[['Golfer','Tournament','Course','Date 4','Score 4']]
-    #df1.columns = df2.columns = df3.columns = df4.columns = ['Golfer','Tournament','Course','Date','Score']
-    #df = pd.concat([df1,df2,df3,df4])
-    #df = df.reset_index(drop=True)
-    #df.to_csv(r'C:\Users\Mitch\Projects\Golf\Data\individual_rounds.csv',index=False)
+    print(df)
+    df.to_csv(r'C:\Users\Mitch\Projects\Golf\Data\individual_rounds.csv',index=False)
 
     #cdf = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\Courses.csv', index_col = 'Unnamed: 0')
     #cdf = cdf[['Course Name','Latitude','Longitude']]
