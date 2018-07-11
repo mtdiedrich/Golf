85d6fe2fefaf0947377f64b3154d7fd34adf5775
mtdiedrich <mitchdiedrich@gmail.com>
2018-07-10 23:47:18 -0500

    major rewrite begun

--- timezones.py
+++ timezones.py
@@ -5,31 +5,35 @@ import sys
 from datetime import datetime as dt
 
 pd.set_option('display.max_columns',20)
-pd.set_option('display.width',1000)
+pd.set_option('display.max_rows',200)
 GMAPS = googlemaps.Client(key=input("Key: ")) #input Google API key
 
+def timezone_data(row):
+    print(row['Latitude'],row['Longitude'])
+    timezone = tz.timezone(GMAPS,location=(row['Latitude'],row['Longitude']))
+    zone = None
+    try:
+        zone = int(timezone['rawOffset']/3600)
+    except ValueError:
+        print(timezone)
+    return zone
+
 def geo_tuple(row):
-    lat = row[2]
-    lng = row[3]
-    return tuple((lat,lng))
+    lat = row['Latitude']
+    lng = row['Longitude']
+    return tuple(lat,lng)
 
 def main():
-    #This need rewriten because the input file no longer exists
-    
-    df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\courses_dates_geodata.csv', index_col = 'Unnamed: 0')
-    df['Pairs'] = df.apply(lambda row: geo_tuple(row), axis = 1)
-    geodata = list(set(list(df['Pairs'])))
-    timezones = []
-    for x in range(len(geodata)):
-        timezone = tz.timezone(GMAPS,geodata[x])
-        timezones += [[geodata[x][0],geodata[x][1],timezone['timeZoneId'],timezone['timeZoneName'],int(timezone['rawOffset'])/3600]]
-        try:
-            int(timezone['rawOffset']/3600)
-        except ValueError:
-            print(timezone)
-    tdf = pd.DataFrame(timezones)
-    print(tdf)
-    tdf.to_csv(r'C:\Users\Mitch\Projects\Golf\Data\timezones.csv')
 
+    cols = ['Golf','Tournament','Course','Date','Score','Latitude','Longitude','Region','Timezone','UTC Offset']
+    types = [str,str,str,str,float,float,float,str,str,float]
+    data_types = dict(zip(cols,types))
+    df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\full_data.csv',dtype=data_types,encoding='latin1')
+    ndf = df[['Latitude','Longitude']]
+    ndf = ndf.drop_duplicates()
+    #Drop nan from ndf
+    #ndf['UTC Offset'] = ndf.apply(lambda row: timezone_data(row),axis=1)
+    print(ndf)
+    
 if __name__=="__main__":
     main()
