3c04d292e969beaa34403db705da548ce60d73ee
mtdiedrich <mitchdiedrich@gmail.com>
2018-07-12 19:16:25 -0500

    correlation matrix

--- Regression.py
+++ Regression.py
@@ -8,38 +8,31 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 pd.set_option('display.max_columns',20)
 pd.set_option('display.width',1000)
+pd.set_option('display.max_colwidth',10)
 
 def main():
 
     df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\full_data.csv',encoding='latin1')
-    df = df.rename(columns={'Precipitation Intensity':'Precip Intens','Precipitation Probability':'Precip Prob'})
 
-    features = [f for f in df.columns]
-    features = [features[4]] + [features[8]] + [features[10]] + features[12:]
-
-    ndf = df[features]
-    ndf = ndf.dropna()
-    ndf = ndf.reset_index(drop=True)
-    ndf = ndf.sample(frac=1)
-    
-    #corr_mat = ndf.corr()
-    #print(corr_mat)
+    corr_mat = df.corr()
+    corr_mat.columns = [f[0:4] for f in corr_mat.columns]
+    print(corr_mat)
     
     #Everything broke because you cahnged the format of the input data. For shame.
-    x = ndf[features[1:]]
-    x = np.asarray(x).reshape(x.shape[0],x.shape[1])
-    y = ndf[features[0]]
-    y = np.asarray(y).reshape(y.shape[0],1)
+    #x = ndf[features[1:]]
+    #x = np.asarray(x).reshape(x.shape[0],x.shape[1])
+    #y = ndf[features[0]]
+    #y = np.asarray(y).reshape(y.shape[0],1)
 
-    linreg = linear_model.LinearRegression()
-    linreg.fit(x,y)
+    #linreg = linear_model.LinearRegression()
+    #linreg.fit(x,y)
 
-    pred = linreg.predict(x)
-    residuals = pred - y
+    #pred = linreg.predict(x)
+    #residuals = pred - y
    
-    print(np.mean(residuals))
-    print(np.std(residuals))
-    print(np.histogram(residuals))
+    #print(np.mean(residuals))
+    #print(np.std(residuals))
+    #print(np.histogram(residuals))
     
     #mse = mean_squared_error(test_y, pred)
     #print(linreg.intercept_,linreg.coef_,mse)
