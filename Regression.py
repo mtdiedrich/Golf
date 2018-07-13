e5bbee79e2c3090e0cce7e98f49892e95b39c9d4
mtdiedrich <mitchdiedrich@gmail.com>
2018-07-13 15:10:03 -0500

    structure and planning

--- Regression.py
+++ Regression.py
@@ -10,9 +10,6 @@ pd.set_option('display.max_columns',20)
 pd.set_option('display.width',1000)
 pd.set_option('display.max_colwidth',10)
 
-def replace_nan_with_median():
-    x = 0
-
 def replace_nan_with_linreg():
     x = 0
 
@@ -42,14 +39,17 @@ def main():
     
     #BUILD DATA
     numeric_columns = df.select_dtypes(include=[int,float]).columns
+    col_data = ['UTC Offset','Precipitation Intensity',
+                'Precipitation Intensity Error','Precipitation Probability',
+                'Temperature','Apparent Temperature','Dew Point','Humidity',
+                'Pressure','Wind Speed','Wind Gust','Wind Bearing',
+                'Cloud Cover','Visibility']
+
+    #WRITE A METHOD THAT TAKES IN THE FRAME AND RETURNS (X,Y)
 
     #Nan drop
     na_drop_df= df.dropna()
-    na_drop_x = na_drop_df[['UTC Offset','Precipitation Intensity',
-            'Precipitation Intensity Error','Precipitation Probability',
-            'Temperature','Apparent Temperature','Dew Point','Humidity',
-            'Pressure','Wind Speed','Wind Gust','Wind Bearing',
-            'Cloud Cover','Visibility']]
+    na_drop_x = na_drop_df[col_data]
     na_drop_x = np.asarray(na_drop_x).reshape(na_drop_x.shape[0],na_drop_x.shape[1])
     na_drop_y = na_drop_df[['Score']]
     na_drop_y = np.asarray(na_drop_y).reshape(na_drop_y.shape[0],1)
@@ -58,9 +58,34 @@ def main():
     mean_df = df
     for x in numeric_columns:
         mean_df[x] = mean_df[x].fillna(mean_df[x].mean())
+    mean_x = mean_df[col_data]
+    mean_x = np.asarray(mean_x).reshape(mean_x.shape[0],mean_x.shape[1])
+    mean_y = mean_df[['Score']]
+    mean_y = np.asarray(mean_y).reshape(mean_y.shape[0],1)
     
-    print(mean_df)
-
+    #Nan mean replacement by course
+    #Nan mean replacement by tournament
+    #Nan median replacement
+    #Nan median replacement by course
+    #Nan median replacement by tournament
+    #Nan linreg replacement
+    #Nan linreg replacement by course
+    #Nan linreg replacement by tournament
+    #Nan multreg replacement
+    #Nan multreg replacement by course
+    #Nan multreg replacement by tournament
+    #Nan ridgereg replacement
+    #Nan ridgereg replacement by course
+    #Nan ridgereg replacement by tournament
+    #Nan lasso replacement
+    #Nan lasso replacement by course
+    #Nan lasso replacement by tournament
+    #Nan sgd replacement
+    #Nan sgd replacement by course
+    #Nan sgd replacement by tournament
+    #Nan svr replacement
+    #Nan svr replacement by course
+    #Nan svr replacement by tournament
 
     #linreg = linear_model.LinearRegression()
     #linreg.fit(na_drop_x,na_drop_y)
