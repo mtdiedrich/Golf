dbb92d42bf8c2b3c7fb54c95a00e30606fa0c84b
mtdiedrich <mitchdiedrich@gmail.com>
2018-07-13 14:58:37 -0500

    mean replacement

--- Regression.py
+++ Regression.py
@@ -10,10 +10,7 @@ pd.set_option('display.max_columns',20)
 pd.set_option('display.width',1000)
 pd.set_option('display.max_colwidth',10)
 
-def replace_nan_with_means():
-    x = 0
-
-def replace_nan_with_medians():
+def replace_nan_with_median():
     x = 0
 
 def replace_nan_with_linreg():
@@ -43,38 +40,44 @@ def main():
     print(corr_mat)
     print() 
     
-    #Build data
-    
-    #Handle Nan with various estimations
-    #Via average
-    #Via prediction from other attributes
-    df_na_drop = df.dropna()
-    x = df_na_drop[['UTC Offset','Precipitation Intensity',
+    #BUILD DATA
+    numeric_columns = df.select_dtypes(include=[int,float]).columns
+
+    #Nan drop
+    na_drop_df= df.dropna()
+    na_drop_x = na_drop_df[['UTC Offset','Precipitation Intensity',
             'Precipitation Intensity Error','Precipitation Probability',
             'Temperature','Apparent Temperature','Dew Point','Humidity',
             'Pressure','Wind Speed','Wind Gust','Wind Bearing',
             'Cloud Cover','Visibility']]
-    x = np.asarray(x).reshape(x.shape[0],x.shape[1])
-    y = df_na_drop[['Score']]
-    y = np.asarray(y).reshape(y.shape[0],1)
+    na_drop_x = np.asarray(na_drop_x).reshape(na_drop_x.shape[0],na_drop_x.shape[1])
+    na_drop_y = na_drop_df[['Score']]
+    na_drop_y = np.asarray(na_drop_y).reshape(na_drop_y.shape[0],1)
 
-    linreg = linear_model.LinearRegression()
-    linreg.fit(x,y)
-
-    pred = linreg.predict(x)
-    residuals = pred - y
-
-    print("RESIDUAL MEAN")
-    print(np.mean(residuals))
-    print("RESIDUAL STD")
-    print(np.std(residuals))
-    print()
-    for x in range(len(np.histogram(residuals,bins=[x for x in range(-15,20)])[0])):
-        first = np.histogram(residuals,bins=[x for x in range(-15,20)])[0][x]
-        second = np.histogram(residuals,bins=[x for x in range(-15,20)])[1][x]
-        print(first,second)
-        
+    #Nan mean replacement
+    mean_df = df
+    for x in numeric_columns:
+        mean_df[x] = mean_df[x].fillna(mean_df[x].mean())
     
+    print(mean_df)
+
+
+    #linreg = linear_model.LinearRegression()
+    #linreg.fit(na_drop_x,na_drop_y)
+
+    #pred = linreg.predict(na_drop_x)
+    #residuals = pred - na_drop_y
+
+    #print("RESIDUAL MEAN")
+    #print(np.mean(residuals))
+    #print("RESIDUAL STD")
+    #print(np.std(residuals))
+    #print()
+    #for x in range(len(np.histogram(residuals,bins=[x for x in range(-15,20)])[0])):
+        #first = np.histogram(residuals,bins=[x for x in range(-15,20)])[0][x]
+        #second = np.histogram(residuals,bins=[x for x in range(-15,20)])[1][x]
+        #print(first,second)
+        
     #mse = mean_squared_error(test_y, pred)
     #print(linreg.intercept_,linreg.coef_,mse)
 
