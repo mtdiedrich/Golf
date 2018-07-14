fd408df975aaca132047988b33e3658703fc7351
mtdiedrich <mitchdiedrich@gmail.com>
2018-07-13 23:04:38 -0500

    11 on a Friday update

--- Regression.py
+++ Regression.py
@@ -19,6 +19,9 @@ def return_x_and_y(frame):
     y = np.asarray(y).reshape(y.shape[0],1)
     return x, y
 
+def replace_nan_multiple_regression(data):
+    print(data)
+
 def main():
 
     cols = ['Golfer','Tournament','Course','Date','Score','Latitude',
@@ -32,66 +35,34 @@ def main():
     data_types = dict(zip(cols,types))
     df = pd.read_csv(r'C:\Users\Mitch\Projects\Golf\Data\full_data.csv',dtype=data_types,encoding='latin1')
 
+    numeric_columns = df.select_dtypes(include=[int,float]).columns
+    linreg = linear_model.LinearRegression()
+    y = return_x_and_y(df)[1]
+
     corr_mat = df.corr()
     corr_mat.columns = [f[0:8] for f in corr_mat.columns]
     print(corr_mat)
     print() 
-    
+
     #BUILD DATA
-    numeric_columns = df.select_dtypes(include=[int,float]).columns
-
-    #WRITE A METHOD THAT TAKES IN THE FRAME AND RETURNS (X,Y)
-
-    #Nan drop
+    #Nan drop    
     na_drop_df= df.dropna()
     na_drop_x = return_x_and_y(na_drop_df)[0]
     na_drop_y = return_x_and_y(na_drop_df)[1]
+    na_linreg = linear_model.LinearRegression()
+    na_linreg.fit(na_drop_x,na_drop_y)
 
     #Nan mean replacement
     mean_df = df.fillna(df[numeric_columns].mean())
     mean_x = return_x_and_y(mean_df)[0]
-    mean_y = return_x_and_y(mean_df)[1]
+    mean_linreg = linear_model.LinearRegression()
+    mean_linreg.fit(mean_x,y)
 
     #Nan median replacement
     median_df = df.fillna(df[numeric_columns].median())
     median_x = return_x_and_y(median_df)[0]
-    median_y = return_x_and_y(median_df)[1]
-
-    print(median_y)
-
-    #print('here')
-    #mean_df = df
-    #for x in numeric_columns:
-        #mean_df[x] = mean_df[x].fillna(mean_df[x].mean())
-    #mean_x = return_x_and_y(mean_df[numeric_columns])[0]
-    #mean_y = return_x_and_y(mean_df[numeric_columns])[1]
-    #print(mean_df)
-
-    #Nan mean replacement by course
-    #Nan mean replacement by tournament
-    #Nan median replacement by course
-    #Nan median replacement by tournament
-    #Nan linreg replacement
-    #Nan linreg replacement by course
-    #Nan linreg replacement by tournament
-    #Nan multreg replacement
-    #Nan multreg replacement by course
-    #Nan multreg replacement by tournament
-    #Nan ridgereg replacement
-    #Nan ridgereg replacement by course
-    #Nan ridgereg replacement by tournament
-    #Nan lasso replacement
-    #Nan lasso replacement by course
-    #Nan lasso replacement by tournament
-    #Nan sgd replacement
-    #Nan sgd replacement by course
-    #Nan sgd replacement by tournament
-    #Nan svr replacement
-    #Nan svr replacement by course
-    #Nan svr replacement by tournament
-
-    #linreg = linear_model.LinearRegression()
-    #linreg.fit(na_drop_x,na_drop_y)
+    median_linreg = linear_model.LinearRegression()
+    median_linreg.fit(mean_x,y)
 
     #pred = linreg.predict(na_drop_x)
     #residuals = pred - na_drop_y
