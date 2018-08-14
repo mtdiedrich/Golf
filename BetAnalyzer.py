d426ee11f79a009f0aa9a7884d4c1a04bfb6af59
mtdiedrich <mitchdiedrich@gmail.com>
2018-08-14 11:05:03 -0500

    display modifications

--- BetAnalyzer.py
+++ BetAnalyzer.py
@@ -21,35 +21,39 @@ def implied_probabilities(values):
 def kelly_criterion(data):
     kelly = []
     for x in data:
-        kelly += [np.sqrt(np.sqrt(100*(x[3]-x[2])/(1-x[2])))]
+        kelly += [np.sqrt(np.sqrt(100*(x[4]-x[3])/(1-x[3])))]
     return kelly
 
 def main():
 
     logreg = LogisticRegressionModel.Model(r'C:\Users\Mitch\Projects\Golf\Data\Lines.csv')
+    cont = True
     try:
         evals = logreg.evaluate()
     except AttributeError:
         print('Resolve above issue before continuing')
-    implied = implied_probabilities(np.asarray(logreg.lines))
-    ip = pd.DataFrame(implied)
-    ip.columns = ['First Implied','Second Implied']
-    df = pd.concat([logreg.lines,ip],axis=1)
-    first_df = df[['First Golfer','First Odds','First Implied']]
-    second_df = df[['Second Golfer','Second Odds','Second Implied']]
-    cols = ['Golfer','Odds','Implied']
-    first_df.columns = cols
-    second_df.columns = cols
-    df = pd.concat([first_df,second_df])
-    df = df.reset_index(drop=True)
-    projections = {}
-    for x in evals:
-        for y in x.keys():
-            projections[y] = x[y]
-    df['Prediction'] = [projections[x] for x in list(df['Golfer'])]
-    df['Kelly'] = kelly_criterion(np.asarray(df))
-    df = df.dropna()
-    print(df)
+        cont = False
+    if cont:
+        implied = implied_probabilities(np.asarray(logreg.lines))
+        ip = pd.DataFrame(implied)
+        ip.columns = ['First Implied','Second Implied']
+        df = pd.concat([logreg.lines,ip],axis=1)
+        first_df = df[['First Golfer','Second Golfer','First Odds','First Implied']]
+        second_df = df[['Second Golfer','First Golfer','Second Odds','Second Implied']]
+        cols = ['Golfer','Matchup','Odds','Implied']
+        first_df.columns = cols
+        second_df.columns = cols
+        df = pd.concat([first_df,second_df])
+        df = df.reset_index(drop=True)
+        projections = {}
+        for x in evals:
+            for y in x.keys():
+                projections[y] = x[y]
+        df['Prediction'] = [projections[x] for x in list(df['Golfer'])]
+        df['Kelly'] = kelly_criterion(np.asarray(df))
+        df['Date'] = [list(logreg.lines['Date'])[0] for x in range(df.shape[0])]
+        df = df.dropna()
+        print(df)
 
 if __name__=="__main__":
     main()
